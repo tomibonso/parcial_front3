0dc40af6ddcc608523615b78c141b748d213a2d8
tomibonso <tomas.bonsignore@gmail.com>
2023-05-29 22:31:09 -0300

    first commit

---
+++ parcial_front3/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Carga de estudiantes</h1>
+        <form></form>
+        <Card/>
+      </div>
+    </>
+  )
+}
+
+export default App
